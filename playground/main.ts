5b28ddc3443262eed9df9af0f786e010dd200fdf
pzehrel <pzehrel@gmail.com>
2025-08-29 17:40:38 +0800

    refactor: remove commented-out code in main.ts

--- playground/main.ts
+++ playground/main.ts
@@ -1,7 +1,4 @@
 import IMG from './1.png'
 import IMG2 from './2.png'
 
-// console.log(IMG)
-
-// document.getElementById('app')!.innerHTML = '__UNPLUGIN__'
 document.getElementById('app')!.innerHTML = `<img src="${IMG}" /><img src="${IMG2}" />`
