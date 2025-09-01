5c48de3291f05668bfc3c75feafc9ae4b4d37aa3
pzehrel <pzehrel@gmail.com>
2025-09-01 16:10:16 +0800

    feat: consolidate JsquashOptions interface and remove redundant types file

--- src/compressors/jsquash/index.ts
+++ src/compressors/jsquash/index.ts
@@ -1,36 +1,40 @@
+import type { EncodeOptions as AvifOptions } from '@jsquash/avif/meta'
+import type { EncodeOptions as JpegOptions } from '@jsquash/jpeg/meta'
+import type { OptimiseOptions as OxipngOptions } from '@jsquash/oxipng/meta'
+import type { EncodeOptions as WebpOptions } from '@jsquash/webp/meta'
 import { defineCompressor } from '../../compressor'
-import { toArrayBuffer } from '../../utils'
+
 import { compressAvif } from './avif'
 import { compressMozJpeg } from './mozjpeg'
 import { compressOxiPng } from './oxipng'
 import { compressWebp } from './webp'
 
-export type * from './types'
+export const jsquash = defineCompressor<'jsquash'>((ctx) => {
+  return {
+    name: 'jsquash',
+    use: /(jpe?g|png|webp|avif)$/i,
+    compress: async (input, fileType, options) => {
+      const source = ctx.toArrayBuffer(input)
 
-export const jsquash = defineCompressor<'jsquash'>({
-  name: 'jsquash',
-  use: /(jpe?g|png|webp|avif)$/i,
-  compress: async (input, fileType, options) => {
-    const source = toArrayBuffer(input)
-    let output: ArrayBuffer
-
-    switch (fileType.mime) {
-      case 'image/jpeg':
-      case 'image/jpg':
-        output = await compressMozJpeg(source, options?.jsquash?.mozjpeg)
-        break
-      case 'image/png':
-        output = await compressOxiPng(source, options?.jsquash?.oxipng)
-        break
-      case 'image/webp':
-        output = await compressWebp(source, options?.jsquash?.webp)
-        break
-      case 'image/avif':
-        output = await compressAvif(source, options?.jsquash?.avif)
-        break
-      default: return null
-    }
-
-    return output
-  },
+      switch (fileType.mime) {
+        case 'image/jpeg':
+        case 'image/jpg':
+          return await compressMozJpeg(source, options?.jsquash?.mozjpeg)
+        case 'image/png':
+          return await compressOxiPng(source, options?.jsquash?.oxipng)
+        case 'image/webp':
+          return await compressWebp(source, options?.jsquash?.webp)
+        case 'image/avif':
+          return await compressAvif(source, options?.jsquash?.avif)
+        default: return null
+      }
+    },
+  }
 })
+
+export interface JsquashOptions {
+  avif?: Partial<AvifOptions>
+  mozjpeg?: Partial<JpegOptions>
+  oxipng?: Partial<OxipngOptions>
+  webp?: Partial<WebpOptions>
+}
