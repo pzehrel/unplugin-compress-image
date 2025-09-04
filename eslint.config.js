77c87c85e0961a0ae5a1ffe782f1aa5d627e1807
pzehrel <pzehrel@gmail.com>
2025-09-04 11:26:33 +0800

    feat: update ESLint configuration and add eslint-plugin-format dependency

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,11 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   type: 'lib',
 
+  markdown: false,
+  formatters: {
+    markdown: true,
+  },
+
   rules: {
     'ts/ban-ts-comment': 'off',
     'ts/no-namespace': 'off',
